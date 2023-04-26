1d5e5e58fe6b239b9d4cebc0778b3d5b6f0375ae
unknown <fikri.fm12@gmail.com>
2023-04-26 17:04:16 +0700

    menambah api movie

--- MODUL9_1302210053/Controllers/WeatherForecastController.cs
+++ MODUL9_1302210053/Controllers/WeatherForecastController.cs
@@ -1,33 +1,82 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using MODUL9_1302210053;
+
+
 
 namespace MODUL9_1302210053.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    [Route("Api/MOVIES[controller]")]
+    public class MovieController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly List<Movie> _movies = new List<Movie>
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            new Movie
+            {
+                Id = 1,
+                Title = "The Shawshank Redemption",
+                Director = "Frank Darabont",
+                Stars = new List<string>{"Tim Robbins", "Morgan Freeman"},
+                Description = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
+            },
+            new Movie
+            {
+                Id = 2,
+                Title = "The Godfather",
+                Director = "Francis Ford Coppola",
+                Stars = new List<string>{"Marlon Brando", "Al Pacino", "James Caan"},
+                Description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+            },
+            new Movie
+            {
+                Id = 3,
+                Title = "The Dark Knight",
+                Director = "Christopher Nolan",
+                Stars = new List<string>{"Christian Bale", "Heath Ledger", "Aaron Eckhart"},
+                Description = "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
+            }
+        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        [HttpGet]
+        public IEnumerable<Movie> GetMovie()
         {
-            _logger = logger;
+            return _movies;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet("{id}")]
+        public IActionResult GetMovie(int id)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var movie = _movies.FirstOrDefault(m => m.Id == id);
+            if (movie == null)
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return NotFound();
+            }
+            return Ok(movie);
+        }
+
+        [HttpPost]
+        public IActionResult CreateMovie(Movie movie)
+        {
+            movie.Id = _movies.Max(m => m.Id) + 1;
+            _movies.Add(movie);
+            return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMovie(int id)
+        {
+            var movie = _movies.FirstOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            _movies.Remove(movie);
+            return NoContent();
         }
     }
 }
---
+++ MODUL9_1302210053/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace MODUL9_1302210053
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+         public string Title { get; set; }
+         public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+
+
+         //public Movie(string Title , string Director, List<string> stars , string description ) { this.Title = Title; this.Director = Director; this.stars = stars; this.description = description;  }
+
+
+        
+
+    }
+}
